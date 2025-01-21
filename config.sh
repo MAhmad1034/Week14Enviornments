0ce54cc52a939b470a391c3b807383aa61563058
muhammad ahmad <l1f21bsse0597@ucp.edu.pk>
2025-01-21 11:45:10 +0500

    initial commit

---
+++ config.sh
@@ -0,0 +1,9 @@
+echo "Going to configure Repo"
+git branch -m develop
+git config --local user.name "muhammad ahmad"
+git config --local user.email "l1f21bsse0597@ucp.edu.pk"
+git config --local core.editor notepad
+mkdir ./src ./styles
+touch ./styles/index.css ./src/index.html .gitignore
+
+
